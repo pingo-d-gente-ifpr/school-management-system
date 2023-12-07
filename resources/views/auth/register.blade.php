ce035b05a5f912f1529e5f57f78fb987ed47f31c
Allan <alan@crowtech.digital>
2023-12-07 13:52:53 -0300

    change login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,15 +38,14 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
-
+<br>
+        <x-primary-button style="background-color:#ff6b8a; ;color:#ffffff;" class="w-full text-center">
+            {{ __('Register') }}
+        </x-primary-button>
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
