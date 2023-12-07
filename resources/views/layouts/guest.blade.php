7e6e14c4dfe8dd385ff53217226d2bce95134f7f
Allan <allan.g531@hotmail.com>
2023-12-07 00:03:30 -0300

    added assets folder and logo image

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <img src="{{ asset('build/assets/images/logo/logo-com-melancia.png')}}" class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{ asset('assets/images/logo/logo-com-melancia.png')}}" class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
 
