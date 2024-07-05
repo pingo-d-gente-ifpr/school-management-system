9ff61e42451ba3a8b5732c740ba1daa72a0d8e0d
Allan Gabriel <allan.g531@hotmail.com>
2024-07-04 21:30:41 -0300

    added subjects table + validation requests

---
+++ app/Http/Requests/UpdateSubjectRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSubjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['string', 'max:255', 'required'],
+            'start_date' => ['date', 'required'],
+            'end_date' => ['date', 'required'],
+            'photo' => ['nullable', 'max:3072'],
+        ];
+    }
+}
