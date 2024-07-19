19fcea09267ecd55b813eb0abbe55de91b2c3f10
nicpaz <nicoliveeira@gmail.com>
2024-07-18 22:03:54 -0300

    Edit the index user page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -67,7 +67,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link">
+                    <a href="/users" class="nav-link">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi pe-none me-2" viewBox="0 0 16 16">
                             <path
