0447fecaf9c171ab13815ba1d36c79f195223b3c
Allan Gabriel <allan.g531@hotmail.com>
2024-07-24 21:54:05 -0300

    created class table + relational class_subject table

---
+++ database/migrations/2024_07_25_001109_create_classes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('photo')->nullable();
+            $table->enum('period', ['morning','afternoon','full time']);
+            $table->enum('stage', ['maternal','garden I', 'garden II', 'garden III']);
+            $table->timestamps();
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
