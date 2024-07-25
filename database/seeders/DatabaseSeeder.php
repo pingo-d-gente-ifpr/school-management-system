9c7aa73d7d55dd0c779645b71b87ad735b3430bf
Allan Gabriel <allan.g531@hotmail.com>
2024-07-24 21:55:28 -0300

    create class seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminUserSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(SubjectSeeder::class);
+        $this->call(ClasseSeeder::class);
     }
 }
