9acab82775d9f7cd584443b1a17bcc3d1f40180f
Allan Gabriel <allan.g531@hotmail.com>
2024-07-24 21:54:48 -0300

    added validation requests for class

---
+++ app/Http/Requests/UpdateClasseRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\Period;
+use App\Enums\Stage;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateClasseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['string', 'max:255', 'required'],
+            'photo' => ['nullable', 'max:3072'],
+            'period' => ['required', Period::cases()],
+            'stage' => ['required', Stage::cases()],
+        ];
+    }
+}
