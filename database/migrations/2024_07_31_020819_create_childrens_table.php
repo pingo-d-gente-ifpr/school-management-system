09c7d04cb5e4d0cab97b38d2008e6306530eacdb
Allan <allan.g531@hotmail.com>
2024-07-31 13:53:06 -0300

    added childrens table + children_class table

---
+++ database/migrations/2024_07_31_020819_create_childrens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\Gender;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('childrens', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birth_date');
+            $table->string('document')->unique();
+            $table->enum('gender', ['masculino', 'feminino']);
+            $table->boolean('status')->default(true);
+            $table->string('register_number')->unique();
+            $table->string('photo')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('childrens');
+    }
+};
