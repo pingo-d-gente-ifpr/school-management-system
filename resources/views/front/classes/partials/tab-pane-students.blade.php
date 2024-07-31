6229725d4169b1656234996de098c5ab2abd5d41
nicpaz <nicoliveeira@gmail.com>
2024-07-31 15:36:17 -0300

    Created students tab pane

---
+++ resources/views/front/classes/partials/tab-pane-students.blade.php
@@ -0,0 +1,53 @@
+<table class="table table-hover ">
+    <thead>
+        <tr class="align-middle">
+            <th scope="col"></th>
+            <th scope="col">Nome</th>
+            <th scope="col">Matrícula</th>
+            <th scope="col">Status</th>
+            <th scope="col">Modificado em</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($students as $student)
+            <tr class="align-middle">
+                <td>
+                    <img class="rounded-circle" width="50px"
+                        src="{{ $student->photo
+                            ? (Storage::exists('public/' . $student->photo)
+                                ? Storage::url($student->photo)
+                                : asset('assets/' . $student->photo))
+                            : asset('assets/images/logo/user-default.png') }}">
+                </td>
+                <td>{{ $student->name }}</td>
+                <td>{{ $student->register_number }}</td>
+                <td>{{ $student->status }}</td>
+                <td>{{ $student->updated_at->format('d/m/Y') }}</td>
+            </tr>
+            @empty
+            <p> Nenhum aluno matrículado nesta turma.</p>
+        @endforelse
+    </tbody>
+</table>
+<!-- Paginação -->
+<div class="d-flex justify-content-center">
+    {{ $students->withQueryString()->links('components.custom-pagination') }}
+</div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    const activeTab = localStorage.getItem('activeTab');
+    if (activeTab) {
+        const tabElement = document.querySelector(`#myTab button[data-bs-target="${activeTab}"]`);
+        const tab = new bootstrap.Tab(tabElement);
+        tab.show();
+    }
+
+    document.querySelectorAll('#myTab button').forEach(function (tabButton) {
+        tabButton.addEventListener('shown.bs.tab', function (event) {
+            const activeTab = event.target.getAttribute('data-bs-target');
+            localStorage.setItem('activeTab', activeTab);
+        });
+    });
+});
+</script>
