d0ce3725237db7d1e3081276380ebb21075d81fb
nicpaz <nicoliveeira@gmail.com>
2024-07-31 14:47:22 -0300

    Added link to the classes index page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -82,7 +82,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link">
+                    <a  class="nav-link {{ Route::is('classes*') ? 'active' : '' }}" href="{{route('classes.index')}}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi pe-none me-2" viewBox="0 0 16 16">
                             <path
