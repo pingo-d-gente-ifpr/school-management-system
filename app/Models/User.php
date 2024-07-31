570e0c00650402d2ec2de11b66ed12645988d6c0
Allan <allan.g531@hotmail.com>
2024-07-31 13:53:19 -0300

    added childrens relationship in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Subject::class);
     }
+
+    public function childrens(): HasMany
+    {
+        return $this->hasMany(Children::class);
+    }
 }
