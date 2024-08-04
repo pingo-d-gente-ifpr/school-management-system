e13a49caf6627f87b1f0f6d9ae54156a752e7719
Allan <allan.g531@hotmail.com>
2024-08-04 17:36:55 -0300

    fix header import css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,23 +9,12 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.122.0">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
-    <link rel="stylesheet" href="{{ asset('assets/css/headers.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/search.css') }}">
 </head>
 
 <body>
-    <header class="pheader bg-white d-flex py-3 px-3">
+    <header class="pheader bg-white d-flex justify-content-end py-3 px-3">
 
-        <div class="d-lg-flex align-items-center navbar-height justify-content-center w-100 d-none">
-            <form style="width: 50%;" class="search-form search-header d-flex align-items-center">
-                <input id="filter_name" name="q" type="text" class="form-control pl-4 pr-0">
-                <button class="btn" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16"
-                        height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                        <path
-                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85zM6.5 12A5.5 5.5 0 1 1 12 6.5 5.507 5.507 0 0 1 6.5 12z" />
-                    </svg>
-                </button>
-            </form>
-        </div>
 
         <div class="dropdown">
             <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
