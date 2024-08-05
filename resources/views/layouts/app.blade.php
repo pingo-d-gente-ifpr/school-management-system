210a1cea9ca155128ee2764031d318071063c02c
nicpaz <nicoliveeira@gmail.com>
2024-08-05 18:28:41 -0300

    (fix) Add Search to tab pane studentes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,11 @@
 
         <!-- Scripts -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    
 
         {{-- CSS --}}
         <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/search.css') }}">
         
     </head>
     <body class="font-sans antialiased">
