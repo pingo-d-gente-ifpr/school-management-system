6fb3a07054e2fbea2e1a213c7d8f60778d38a6e0
Allan <allan.g531@hotmail.com>
2024-09-15 16:53:21 -0300

    (adjust) added relation with pivot in children and class model

--- app/Models/Children.php
+++ app/Models/Children.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Children extends Model
 {
@@ -28,6 +29,7 @@ class Children extends Model
 
     public function classes(): BelongsToMany
     {
-        return $this->belongsToMany(Classe::class);
+        return $this->belongsToMany(Classe::class)->withPivot('id');
     }
+
 }
