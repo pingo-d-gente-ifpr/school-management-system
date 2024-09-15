cba1acaab4afd93006d74e8c9e9d9138b9ffd611
nicpaz <nicoliveeira@gmail.com>
2024-09-15 20:04:56 -0300

    Remove pagination

--- resources/views/front/classes/partials/tab-pane-students.blade.php
+++ resources/views/front/classes/partials/tab-pane-students.blade.php
@@ -1,6 +1,13 @@
-<x-admin.search/>
+<div class="search-attendance d-flex">
+    <input type="search" class="form-attendance" id="searchStudents" placeholder="Procurar Aluno" aria-label="Procurar Aluno">
+    <button class="btn" type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85zM6.5 12A5.5 5.5 0 1 1 12 6.5 5.507 5.507 0 0 1 6.5 12z"/>
+        </svg>
+    </button>
+</div>
 
-<table class="table table-striped">
+<table class="table table-striped" id="studentsTable">
     <thead>
         <tr class="align-middle">
             <th scope="col"></th>
@@ -33,8 +40,3 @@
         @endforelse
     </tbody>
 </table>
-
-<!-- Paginação -->
-<div class="d-flex justify-content-center">
-    {{ $students->withQueryString()->links('components.custom-pagination') }}
-</div>
