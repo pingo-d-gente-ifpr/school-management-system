11b4295379d1346ddab7f8051bc91c196fafe14e
Nicole Paz <nicoliveeira@gmail.com>
2024-09-17 12:02:55 -0300

    defines user roles navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -68,18 +68,7 @@
                     {{ __('Matérias') }}
                 </a>
             </li>
-            {{-- <li>
-                <a href="#" class="nav-link">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi pe-none me-2" viewBox="0 0 16 16">
-                        <path
-                            d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
-                        <path
-                            d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
-                    </svg>
-                    Eventos
-                </a>
-            </li> --}}
+            @if (Auth::user()->role != 'parents')
             <li>
                 <a class="nav-link {{ Route::is('users*') ? 'active' : '' }}"
                     href="{{ route('users.index') }}">
@@ -91,6 +80,7 @@
                     Usuários
                 </a>
             </li>
+            @endif
             <li>
                 <a class="nav-link {{ Route::is('classes*') ? 'active' : '' }}"
                     href="{{ route('classes.index') }}">
