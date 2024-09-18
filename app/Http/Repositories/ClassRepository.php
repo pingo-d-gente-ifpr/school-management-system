28638c3b24db33b2aa8a4d4184655791c2a01e7f
Allan <allan.g531@hotmail.com>
2024-09-18 02:47:27 -0300

    (feat) added class policy

--- app/Http/Repositories/ClassRepository.php
+++ app/Http/Repositories/ClassRepository.php
@@ -3,12 +3,39 @@
 namespace App\Http\Repositories;
 
 use App\Models\Classe;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class ClassRepository 
 {
 
     public function getAll(array $filter)
     {
+        $user = Auth::user();
+
+        if($user->isParent()){
+            $childrenIds = DB::table('childrens')
+            ->where('user_id', $user->id)
+            ->pluck('id');
+    
+            $classesIds = DB::table('children_classe')
+                ->whereIn('children_id', $childrenIds)
+                ->pluck('classe_id');
+        
+            return Classe::whereIn('id', $classesIds)
+                ->filter($filter)
+                ->paginate(6);
+        }
+
+        if ($user->isTeacher()) {
+            $classes = DB::table('classe_subject')
+                ->where('user_id', $user->id)
+                ->pluck('classe_id');
+
+            return Classe::whereIn('id', $classes)
+                ->filter($filter)
+                ->paginate(6);
+        }
         return Classe::filter($filter)->paginate(6);
     }
 
--- app/Policies/ClassePolicy.php
+++ app/Policies/ClassePolicy.php
@@ -4,7 +4,7 @@ namespace App\Policies;
 
 use App\Models\Classe;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\DB;
 
 class ClassePolicy
 {
@@ -13,15 +13,32 @@ class ClassePolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return $user->isAdmin() || $user->isTeacher() || $user->isParent();
     }
 
     /**
-     * Determine whether the user can view the model.
+     * Determine if the user can view the class.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Class  $class
+     * @return bool
      */
-    public function view(User $user, Classe $classe): bool
-    {
-        //
+    public function view(User $user, Classe $classe)
+    {   
+        if ($user->isParent()) {
+            $childrenIds = DB::table('childrens')
+                ->where('user_id', $user->id)
+                ->pluck('id');
+
+            return DB::table('children_classe')
+                ->whereIn('children_id', $childrenIds)
+                ->where('classe_id', $classe->id)
+                ->exists();
+        }
+        return $user->isAdmin() || DB::table('classe_subject')
+        ->where('classe_id', $classe->id)
+        ->where('user_id', $user->id)
+        ->exists();
     }
 
     /**
@@ -29,7 +46,7 @@ class ClassePolicy
      */
     public function create(User $user): bool
     {
-        //
+        return $user->isAdmin();
     }
 
     /**
@@ -37,13 +54,16 @@ class ClassePolicy
      */
     public function update(User $user, Classe $classe): bool
     {
-        //
+        return $user->isAdmin() || DB::table('classe_subject')
+        ->where('classe_id', $classe->id)
+        ->where('user_id', $user->id)
+        ->exists();
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Classe $classe): bool
+    public function delete(User $user, Classe $classe)
     {
         //
     }
@@ -51,7 +71,7 @@ class ClassePolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Classe $classe): bool
+    public function restore(User $user, Classe $classe)
     {
         //
     }
@@ -59,7 +79,7 @@ class ClassePolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Classe $classe): bool
+    public function forceDelete(User $user, Classe $classe)
     {
         //
     }
