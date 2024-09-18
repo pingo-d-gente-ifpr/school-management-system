9e4657b5e14a456030b246e6c860c0e721e89197
nicpaz <nicoliveeira@gmail.com>
2024-09-18 16:50:34 -0300

    Change Factory names

--- database/factories/SubjectFactory.php
+++ database/factories/SubjectFactory.php
@@ -17,6 +17,34 @@ class SubjectFactory extends Factory
      */
     public function definition(): array
     {
+        $names = [
+            'Atividades Artísticas',
+            'Brincadeiras de Movimento',
+            'Histórias e Contos',
+            'Exploração Sensorial',
+            'Jogo Livre',
+            'Desenvolvimento Motor',
+            'Cantigas e Música',
+            'Contação de Histórias',
+            'Pintura e Desenho',
+            'Jardinagem Infantil',
+            'Exploração da Natureza',
+            'Contação de Fábulas',
+            'Brincadeiras com Água',
+            'Desenvolvimento da Linguagem',
+            'Jogos de Encaixar',
+            'Histórias da Vida Real',
+            'Jogos de Role Play',
+            'Exploração Tátil',
+            'Atividades de Coordenação',
+            'Jogos Educativos',
+            'Atividades de Cores e Formas',
+            'Brincadeiras com Areia',
+            'Desenvolvimento da Socialização',
+            'Histórias de Animais',
+            'Atividades com Massinha',
+        ];
+
         $photos = [
             'images/subjects/1.png',
             'images/subjects/2.png',
@@ -26,7 +54,7 @@ class SubjectFactory extends Factory
         ];
 
         return [
-            'name' => $this->faker->word,
+            'name' => $this->faker->randomElement($names),
             'photo' => $this->faker->randomElement($photos),
         ];
     }
