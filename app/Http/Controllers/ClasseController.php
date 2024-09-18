d804985d9767e9e77424ef298abb6af0904da847
nicpaz <nicoliveeira@gmail.com>
2024-09-18 00:03:27 -0300

    git restore --source=69891e3

--- app/Http/Controllers/ClasseController.php
+++ app/Http/Controllers/ClasseController.php
@@ -14,7 +14,6 @@ use App\Models\Subject;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ClasseController extends Controller
 {
@@ -31,20 +30,24 @@ class ClasseController extends Controller
      */
     public function index(Request $request)
     {
+
         $filter = $request->only('q');
-        $classes = $this->service->index($filter);
-
-        return view('admin.classes.index', compact('classes'));
+    
+        return view('admin.classes.index')->with('classes', $this->service->index($filter));
     }
-
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        $formOptions = $this->service->getFormOptions();
-        return view('admin.classes.create')->with($formOptions);
+        $subjects = Subject::all();
+        $childrens = Children::all();
+        $teachers = User::where('role', Role::teacher)->get();
+        return view('admin.classes.create')
+        ->with('subjects',$subjects)
+        ->with('teachers',$teachers)
+        ->with('childrens',$childrens);
     }
 
     /**
@@ -60,60 +63,76 @@ class ClasseController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Classe $class,  Request $request)
+    public function show(Classe $class)
     {
         $class = $this->service->show($class);
 
         $subjects = $class->subjects()->get();
         $students = $class->childrens()->get();
 
-        $startDate = $request->input('selected_date') ? Carbon::createFromFormat('d/m/Y', $request->input('selected_date')) : Carbon::now();
+        $startDate = Carbon::now();
         $weekDays = [];
         for ($i = 0; $i < 5; $i++) {
             $date = $startDate->copy()->addDays($i);
             $weekDays[$date->format('Y-m-d')] = $date->format('d/m (l)');
-    }
-
-        $frequencies = DB::table('children_classe')
-        ->join('children_frequency', 'children_classe.id', '=', 'children_frequency.children_classe_id')
-        ->where('children_classe.classe_id', $class->id)
-        ->select('children_classe.children_id', 'children_frequency.*')
-        ->get();
-       
-        $studentsFrequencies = [];
-        foreach ($frequencies as $frequency) {
-            $studentsFrequencies[$frequency->children_id][] = $frequency;
         }
-
 
         return view('front.classes.show')
             ->with('class', $class)
             ->with('subjects', $subjects)
             ->with('students', $students)
-            ->with('weekDays', $weekDays)
-            ->with('studentsFrequencies', $studentsFrequencies);
+            
+            ->with('weekDays', $weekDays);
     }
 
     public function registerGrades(Request $request)
     {
-        $scores = [
-            'score1' => $request->input('score1'),
-            'score2' => $request->input('score2'),
-            'score3' => $request->input('score3'),
-            'score4' => $request->input('score4'),
-        ];
-
-        $this->service->registerGrades($scores);
-
+        foreach ($request->input('score1') as $studentId => $subjects) {
+            foreach ($subjects as $subjectId => $score1) {
+                $score2 = $request->input('score2')[$studentId][$subjectId] ?? null;
+                $score3 = $request->input('score3')[$studentId][$subjectId] ?? null;
+                $score4 = $request->input('score4')[$studentId][$subjectId] ?? null;
+                // Atualizar ou criar a nota apenas se os campos não estiverem vazios
+                $childrenSubject = ChildrenSubject::where('children_id', $studentId)
+                    ->where('classe_subject_id', $subjectId)
+                    ->first();
+    
+                if ($childrenSubject) {
+                    if ($score1 !== null) $childrenSubject->score1 = $score1;
+                    if ($score2 !== null) $childrenSubject->score2 = $score2;
+                    if ($score3 !== null) $childrenSubject->score3 = $score3;
+                    if ($score4 !== null) $childrenSubject->score4 = $score4;
+                    $childrenSubject->save();
+                } else {
+                    ChildrenSubject::create([
+                        'children_id' => $studentId,
+                        'classe_subject_id' => $subjectId,
+                        'score1' => $score1,
+                        'score2' => $score2,
+                        'score3' => $score3,
+                        'score4' => $score4
+                    ]);
+                }
+            }
+        }
+    
         return redirect()->back()->with('success', 'Notas atualizadas com sucesso!');
     }
+    
+    
 
-    
-    
+    /**
+     * Show the form for editing the specified resource.
+     */
     public function edit(Classe $class)
     {
-        $formOptions = $this->service->getFormOptions();
-        return view('admin.classes.edit')->with('class', $class)->with($formOptions);
+        $subjects = Subject::all();
+        $childrens = Children::all();
+        $teachers = User::where('role', Role::teacher)->get();
+        return view('admin.classes.edit')->with('class', $class)
+        ->with('subjects',$subjects)
+        ->with('teachers',$teachers)
+        ->with('childrens',$childrens);
     }
 
     /**
@@ -129,17 +148,16 @@ class ClasseController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Classe $class)
-    {   
-        // dd($class);
-        $this->service->destroy($class);
-        return redirect()->route('classes.index')->with('success', 'Turma excluída com sucesso!');;
+    public function destroy(Classe $classe)
+    {
+        $this->service->destroy($classe);
+        return redirect()->route('classes.index');
     }
 
     public function registerFrequency(Request $request)
     {
-        $data = $request->all();
-        $this->service->registerFrequency($data);
+        dd($request->all());
+
         return response()->json(['message' => 'Dados recebidos com sucesso']);
     }
     
--- app/Http/Services/ClassService.php
+++ app/Http/Services/ClassService.php
@@ -3,96 +3,38 @@
 namespace App\Http\Services;
 
 use App\Http\Repositories\ClassRepository;
-use App\Models\ChildrenClasse;
-use App\Models\ChildrenFrequency;
 use App\Models\Classe;
-use Illuminate\Support\Facades\Auth;
-use App\Enums\Role;
 use Carbon\Carbon;
-use App\Models\ChildrenSubject;
-use App\Models\ClasseSubject;
-use App\Models\Children;
-use App\Models\Subject;
-use App\Models\User;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 class ClassService{
 
     protected $repository;
 
-    public function __construct(ClassRepository $repository)
-    {
+    public function __construct(ClassRepository $repository){
         $this->repository = $repository;
     }
 
     public function index(array $filter)
     {
-        $user = Auth::user();
-    
-        if ($user->role == Role::admin->name) {
-            $classes = Classe::paginate(6);
-        }
-        else if ($user->role == Role::teacher->name) {
-            $classes = ClasseSubject::where('user_id', $user->id)
-                                    ->with('classe')
-                                    ->paginate(6); 
-    
-            $classes->getCollection()->transform(function($item) {
-                return $item->classe;
-            })->unique();
-        }
-        else if ($user->role == Role::parents->name) {
-            $childrenClasses = Children::where('user_id', $user->id)
-                                        ->with('classes')
-                                        ->get()
-                                        ->pluck('classes')
-                                        ->flatten()
-                                        ->unique();
-    
-            $classes = $this->paginateCollection($childrenClasses, 6);
-        }
-    
-        return $classes;
+        return $this->repository->getAll($filter);
     }
-
-    public function paginateCollection($items, $perPage = 6, $page = null, $options = [])
-    {
-        $page = $page ?: (LengthAwarePaginator::resolveCurrentPage() ?: 1);
-        $items = $items instanceof Collection ? $items : Collection::make($items);
-        return new LengthAwarePaginator(
-            $items->forPage($page, $perPage),
-            $items->count(),
-            $perPage,
-            $page,
-            $options
-        );
-    }
-
-    
 
     public function store(array $data, $request)
     {
-        if ($request->hasFile('photo')) {
+
+        if($request->hasFile('photo'))
+        {
             $data['photo'] = $request->file('photo')->store('images/classes', 'public');
         }
 
         $class = $this->repository->store($data);
-
-        if (isset($data['subjects']) && !empty($data['subjects'])) {
-            $class->subjects()->attach($data['subjects']);
-        }
-
-        if (isset($data['childrens']) && !empty($data['childrens'])) {
-            $class->childrens()->attach($data['childrens']);
-        }
-
+        $class->subjects()->attach($data['subjects']);
+        $class->childrens()->attach($data['childrens']);
         $class->save();
 
         return $class;
-}
-
+    }
 
     public function show(Classe $class)
     {
@@ -108,8 +50,8 @@ class ClassService{
         }
 
         $class = $this->repository->update($data, $class);
-        $class->subjects()->sync($data['subjects']??[]);
-        $class->childrens()->sync($data['childrens']??[]);
+        $class->subjects()->syncWithoutDetaching($data['subjects']??[]);
+        $class->childrens()->syncWithoutDetaching($data['childrens']??[]);
         $class->save();
 
         return $class;
@@ -120,97 +62,7 @@ class ClassService{
         if($class->photo){
             Storage::disk('public')->delete($class->photo);
         }
-
         return $this->repository->destroy($class);
     }
 
-    public function registerFrequency($data)
-    {
-        $childrenClass = ChildrenClasse::find($data['children_classe_id']);
-        $data['date'] = Carbon::createFromFormat('d/m/Y', $data['date'])->format('Y-m-d');
-        
-        if($data['attendance'] == 'false'){
-            $data['attendance'] = false;
-        }else{
-            $data['attendance'] = true;
-        }
-
-        $frequencyExists = $childrenClass->frequencies()->where('date', $data['date'])->first();
-
-        if(!$frequencyExists){
-            return $childrenClass->frequencies()->create($data);
-        }
-
-        return $frequencyExists->update($data);
-    }
-
-    public function getFormOptions()
-    {
-        $subjects = Subject::all();
-        $childrens = Children::all();
-        $teachers = User::where('role', Role::teacher)->get();
-
-        return compact('subjects', 'teachers', 'childrens');
-    }
-
-    public function getClassDetails(Classe $class)
-    {
-        $subjects = $class->subjects()->get();
-        $students = $class->childrens()->get();
-
-        $startDate = Carbon::now();
-        $weekDays = [];
-        for ($i = 0; $i < 5; $i++) {
-            $date = $startDate->copy()->addDays($i);
-            $weekDays[$date->format('Y-m-d')] = $date->format('d/m (l)');
-        }
-
-        return compact('class', 'subjects', 'students', 'weekDays');
-    }
-
-    public function registerGrades($scores)
-    {
-        foreach ($scores['score1'] as $studentId => $subjects) {
-            foreach ($subjects as $subjectId => $score1) {
-                $score2 = $scores['score2'][$studentId][$subjectId] ?? null;
-                $score3 = $scores['score3'][$studentId][$subjectId] ?? null;
-                $score4 = $scores['score4'][$studentId][$subjectId] ?? null;
-
-                // Garanta que $scoreData tem o formato correto
-                $scoreData = [
-                    'score1' => $score1,
-                    'score2' => $score2,
-                    'score3' => $score3,
-                    'score4' => $score4
-                ];
-
-                // Atualize ou crie a nota
-                $this->updateOrCreateChildrenSubject($studentId, $subjectId, $scoreData);
-            }
-        }
-    }
-
-    private function updateOrCreateChildrenSubject($studentId, $subjectId, $scoreData)
-    {
-        $childrenSubject = ChildrenSubject::where('children_id', $studentId)
-            ->where('classe_subject_id', $subjectId)
-            ->first();
-    
-        if ($childrenSubject) {
-            if (isset($scoreData['score1']) && $scoreData['score1'] !== null) $childrenSubject->score1 = $scoreData['score1'];
-            if (isset($scoreData['score2']) && $scoreData['score2'] !== null) $childrenSubject->score2 = $scoreData['score2'];
-            if (isset($scoreData['score3']) && $scoreData['score3'] !== null) $childrenSubject->score3 = $scoreData['score3'];
-            if (isset($scoreData['score4']) && $scoreData['score4'] !== null) $childrenSubject->score4 = $scoreData['score4'];
-            $childrenSubject->save();
-        } else {
-            ChildrenSubject::create([
-                'children_id' => $studentId,
-                'classe_subject_id' => $subjectId,
-                'score1' => $scoreData['score1'],
-                'score2' => $scoreData['score2'],
-                'score3' => $scoreData['score3'],
-                'score4' => $scoreData['score4']
-            ]);
-        }
-    }    
 }
